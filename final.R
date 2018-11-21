4cf9792cfce9482ec8fbbfe575818510e4b22103
fguibourt <44946638+fguibourt@users.noreply.github.com>
2018-11-21 17:30:56 +0100

    Add files via upload

---
+++ final.R
@@ -0,0 +1,27 @@
+#########################
+######### SETUP #########
+#########################
+
+## libraries
+library(ggplot2)
+library(dplyr)
+
+## set working directory (change it for your computer)
+setwd("C:/Users/Fratoi/Documents/Cours/Costa Rica")
+
+## load data
+data = read.csv("train.csv/train.csv", sep = ",", header = T)
+data = data.frame(data)
+
+#########################
+########## EDA ##########
+#########################
+
+
+
+#########################
+## DATA PREPARATION #####
+#########################
+
+## recode data
+

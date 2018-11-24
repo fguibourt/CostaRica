37d511d3bb6cbba50aafd50f39f63baca279cecf
fguibourt <44946638+fguibourt@users.noreply.github.com>
2018-11-24 14:13:34 +0100

    Update data_preparation.R

--- data_preparation.R
+++ data_preparation.R
@@ -4,7 +4,10 @@
 
 ## libraries
 library(dplyr)
+library(magrittr)
 library(corrplot)
+library(FactoMineR)
+library(factoextra)
 library(randomForest)
 library(ggplot2)
 
@@ -19,7 +22,37 @@ data$Target = factor(data$Target)
 data = data %>%
   mutate(dependency = 
            ifelse(dependency == "yes", 1,
-                  ifelse(dependency == "no", 0, as.numeric(as.character(dependency)))))
+                    ifelse(dependency == "no", 0, as.numeric(as.character(dependency)))),
+           v18q1 = 
+             ifelse(v18q == 0,0,v18q1),
+           v2a1 = 
+             ifelse(tipovivi1==1,0,v2a1),
+           v2a1_missing =
+             ifelse(is.na(v2a1),"True","False"),
+           rez_esc = 
+             ifelse((age<7)|(age>19),0,rez_esc),
+           rez_esc_missing = 
+             ifelse(is.na(rez_esc),"True","False"),
+           edjefi = 
+             ifelse(edjefe=='yes',1,
+                    ifelse(edjefe=='no',0,as.numeric(as.character(edjefe)))) 
+           + ifelse(edjefa=='yes',1,
+                    ifelse(edjefa=='no',0,as.numeric(as.character(edjefa)))))
+
+## convert to factors
+factors = c('hacdor','hacapo','v14a','refrig','v18q','paredblolad','paredzocalo','paredpreb','pareddes','paredmad',
+            'paredzinc','paredfibras','paredother','pisomoscer','pisocemento','pisoother','pisonatur','pisonotiene','pisomadera',
+            'techozinc','techoentrepiso','techocane','techootro','cielorazo','abastaguadentro','abastaguafuera','abastaguano','public',
+            'planpri','noelec','coopele','sanitario1','sanitario2','sanitario3','sanitario5','sanitario6','energcocinar1','energcocinar2',
+            'energcocinar3','energcocinar4','elimbasu1','elimbasu2','elimbasu3','elimbasu4','elimbasu5','elimbasu6','epared1','epared2',
+            'epared3','etecho1','etecho2','etecho3','eviv1','eviv2','eviv3','dis','male','female','estadocivil1','estadocivil2','estadocivil3',
+            'estadocivil4','estadocivil5','estadocivil6','estadocivil7','parentesco1','parentesco2','parentesco3','parentesco4','parentesco5',
+            'parentesco6','parentesco7','parentesco8','parentesco9','parentesco10','parentesco11','parentesco12','instlevel1','instlevel2',
+            'instlevel3','instlevel4','instlevel5','instlevel6','instlevel7','instlevel8','instlevel9','tipovivi1','tipovivi2','tipovivi3',
+            'tipovivi4','tipovivi5','computer','television','mobilephone','lugar1','lugar2','lugar3','lugar4','lugar5','lugar6','area1','area2')
+
+data %<>%
+  mutate_each_(funs(factor(.)),factors)
 
 ##################################################
 ################################### EDA ##########
@@ -42,7 +75,7 @@ t = table(estadocivil3)
 round(prop.table(t),2)
 barplot(t)
 
-  ## vs. Target
+## vs. Target
 tt = table(estadocivil3, Target)
 round(prop.table(tt),2)
 round(prop.table(tt,2),2)
@@ -55,7 +88,7 @@ boxplot(dependency)
 summary(dependency)
 var(dependency)
 
-  ## vs. Target
+## vs. Target
 boxplot(dependency~Target)
 tapply(dependency, Target, summary)
 tapply(dependency, Target, sd)
@@ -72,7 +105,7 @@ t = table(table_lugar$region)
 round(prop.table(t),2)
 barplot(t)
 
-  ## vs. Target
+## vs. Target
 
 tt = table(data$Target,table_lugar$region)
 tt
@@ -81,7 +114,32 @@ barplot(prop.table(table(data$Target,table_lugar$region),2))
 
 chisq.test(tt)
 
-## corrplot
+################# MULTIVARIATE ANALYSIS #############
+
+## FULL CORRPLOT
+data_corr1 = data %>%
+  select_if(is.numeric)
+
+corr1 = cor(data_corr1, use = "complete.obs")
+corrplot(corr1, order = "hclust")
+
+## CLEAN CORRPLOT
+data_corr2 = data %>%
+  select_if(is.numeric) %>%
+  select(1:3,15:17,22:27,37)
+
+corr2 = cor(data_corr2, use = "complete.obs")
+corrplot(corr2, order = "hclust")
+
+## ACP
+# data_corr1$Target = data$Target
+# data_corr2$Target = data$Target
+
+acp1 = PCA(data_corr1, scale.unit = T, ncp = 6, graph = T)
+acp2 = PCA(data_corr2, scale.unit = T, ncp = 6, graph = T)
+
+fviz_eig(acp2)
+fviz_pca_var(acp2, col.var = "contrib", select.var = list(contrib = 8))
 
 ## to be completed
 
@@ -90,10 +148,16 @@ chisq.test(tt)
 ########################### DATA PREPARATION #####
 ##################################################
 
-#################### CLEANING ####################
+## load data
+data = as_data_frame(read.csv("train.csv/train.csv", sep = ",", header = T))
+data$Target = factor(data$Target)
 
-data_temp %>%
-  mutate(v18q1 = 
+## clean data
+data = data %>%
+  mutate(dependency = 
+           ifelse(dependency == "yes", 1,
+                  ifelse(dependency == "no", 0, as.numeric(as.character(dependency)))),
+         v18q1 = 
            ifelse(v18q == 0,0,v18q1),
          v2a1 = 
            ifelse(tipovivi1==1,0,v2a1),
@@ -102,8 +166,12 @@ data_temp %>%
          rez_esc = 
            ifelse((age<7)|(age>19),0,rez_esc),
          rez_esc_missing = 
-           ifelse(is.na(rez_esc),"True","False"))
-
+           ifelse(is.na(rez_esc),"True","False"),
+         edjefi = 
+           ifelse(edjefe=='yes',1,
+                  ifelse(edjefe=='no',0,as.numeric(as.character(edjefe)))) 
+         + ifelse(edjefa=='yes',1,
+                  ifelse(edjefa=='no',0,as.numeric(as.character(edjefa)))))
 
 ## data : original data
 ## data_temp : original data + features aggregated at the individual granularity
@@ -135,116 +203,108 @@ data_temp %>%
 
 data_temp = data %>%
   mutate(parent_family = 
-                       parentesco1 +
-                       parentesco2 + 
-                       parentesco3 + 
-                       parentesco6 + 
-                       parentesco7 + 
-                       parentesco9 + 
-                       parentesco11,
+           parentesco1 +
+           parentesco2 + 
+           parentesco3 + 
+           parentesco6 + 
+           parentesco7 + 
+           parentesco9 + 
+           parentesco11,
          parent_inlaw = 
-                       parentesco4 + 
-                       parentesco5 + 
-                       parentesco8 + 
-                       parentesco10,
+           parentesco4 + 
+           parentesco5 + 
+           parentesco8 + 
+           parentesco10,
          parent_exte = 
-                       parentesco12,
+           parentesco12,
          wall_score =
-                       epared1 * 1 +
-                       epared2 * 2 +
-                       epared3 * 3,
+           epared1 * 1 +
+           epared2 * 2 +
+           epared3 * 3,
          roof_score = 
-                       etecho1 * 1 +
-                       etecho2 * 2 +
-                       etecho3 * 3,
+           etecho1 * 1 +
+           etecho2 * 2 +
+           etecho3 * 3,
          floor_score = 
-                       eviv1 * 1 +
-                       eviv2 * 2 + 
-                       eviv3 * 3,
+           eviv1 * 1 +
+           eviv2 * 2 + 
+           eviv3 * 3,
          score_wall_material = 
-                       paredblolad*6 
-                     + paredzocalo*4 
-                     + paredpreb*5 
-                     + pareddes*1 
-                     + paredmad*4 
-                     + paredzinc*4 
-                     + paredfibras*2 
-                     + paredother*2,
+           paredblolad*6 
+         + paredzocalo*4 
+         + paredpreb*5 
+         + pareddes*1 
+         + paredmad*4 
+         + paredzinc*4 
+         + paredfibras*2 
+         + paredother*2,
          score_floor_material = 
-                       pisomoscer*5
-                     + pisocemento*4
-                     + pisoother*4
-                     + pisonatur*1
-                     + pisonotiene*1
-                     + pisomadera*3,
+           pisomoscer*5
+         + pisocemento*4
+         + pisoother*4
+         + pisonatur*1
+         + pisonotiene*1
+         + pisomadera*3,
          score_roof_material = 
-                      techozinc*2
-                     +techoentrepiso*2
-                     +techocane*1
-                     +techootro*4,
+           techozinc*2
+         +techoentrepiso*2
+         +techocane*1
+         +techootro*4,
          education_score = 
-                       instlevel1 * 0 +
-                       instlevel2 * 1 +
-                       instlevel3 * 2 +
-                       instlevel4 * 3 +
-                       instlevel5 * 4 +
-                       instlevel6 * 5 +
-                       instlevel7 * 6 +
-                       instlevel8 * 7 +
-                       instlevel9 * 8,
+           instlevel1 * 0 +
+           instlevel2 * 1 +
+           instlevel3 * 2 +
+           instlevel4 * 3 +
+           instlevel5 * 4 +
+           instlevel6 * 5 +
+           instlevel7 * 6 +
+           instlevel8 * 7 +
+           instlevel9 * 8,
          malus = 
-                       sanitario1 +
-                       noelec +
-                       pisonotiene +
-                       pisonatur +
-                       techocane +
-                       pareddes +
-                       abastaguano +
-                       cielorazo +
-                       elimbasu3 +
-                       energcocinar4 +
-                       tipovivi4,
-         
+           sanitario1 +
+           noelec +
+           pisonotiene +
+           pisonatur +
+           techocane +
+           pareddes +
+           abastaguano +
+           cielorazo +
+           elimbasu3 +
+           energcocinar4 +
+           tipovivi4,
          confort = 
-                       coalesce(
-                       refrig +
-                       computer + 
-                       v18q1 + 
-                       television,0L),
+             refrig +
+               computer + 
+               v18q1 + 
+               television,
          children =
-                      ifelse(age < 8, 1, 0),
+           ifelse(age < 8, 1, 0),
          student = 
-                      ifelse(age >= 8 & age < 20, 1, 0), 
+           ifelse(age >= 8 & age < 20, 1, 0), 
          worker = 
-                      ifelse(age >=20 & age < 65, 1 , 0), 
+           ifelse(age >=20 & age < 65, 1 , 0), 
          old = 
-                      ifelse(age >= 64, 1, 0),
-         edjefi = 
-                      ifelse(edjefe=='yes',1,
-                      ifelse(edjefe=='no',0,as.numeric(as.character(edjefe)))) 
-                      + ifelse(edjefa=='yes',1,
-                      ifelse(edjefa=='no',0,as.numeric(as.character(edjefa)))),
+           ifelse(age >= 64, 1, 0),
          urban =
-                      area1,
-         
+           area1,
          score_elec = 
-                      noelec*0 + 
-                      coopele*1 + 
-                      public*2 + 
-                      planpri*3,
+           noelec*0 + 
+           coopele*1 + 
+           public*2 + 
+           planpri*3,
          phone_per_capita = 
-                      qmobilephone/tamviv,
+           qmobilephone/tamviv,
          tablet_per_capital = 
-                      ifelse(is.na(v18q1/tamviv),0,v18q1/tamviv),
+           ifelse(is.na(v18q1/tamviv),0,v18q1/tamviv),
          rooms_per_capital = 
-                      rooms/tamviv,
+           rooms/tamviv,
          rent_per_capital = 
-                      ifelse(is.na(v2a1/tamviv),0,v2a1/tamviv),
+           ifelse(is.na(v2a1/tamviv),0,v2a1/tamviv),
          meaneduc = 
-                      ifelse(is.na(meaneduc), 0, meaneduc),
+           ifelse(is.na(meaneduc), 0, meaneduc),
          SQBmeaned = 
-                      ifelse(is.na(meaneduc), 0, meaneduc)
-         )
+           ifelse(is.na(meaneduc), 0, meaneduc)
+  )
 
 
 ################### DATA GROUP #################### 
@@ -254,38 +314,38 @@ data_group = data_temp %>%
   group_by(idhogar) %>%
   summarise(
     nb_parentfamily = 
-          sum(parent_family),
+      sum(parent_family),
     nb_parentinlaw = 
-          sum(parent_inlaw),
+      sum(parent_inlaw),
     nb_parentexte = 
-          sum(parent_exte),
+      sum(parent_exte),
     nb_children = 
-          sum(children),
+      sum(children),
     nb_student = 
-          sum(student),
+      sum(student),
     nb_worker = 
-          sum(worker),
+      sum(worker),
     nb_old = 
-          sum(old),
+      sum(old),
     sum_escolari = 
-          sum(escolari),
+      sum(escolari),
     sum_rez_esc = 
-          sum(rez_esc, na.rm = T),
+      sum(rez_esc, na.rm = T),
     sum_dis = 
-          sum(dis, na.rm = T),
+      sum(dis, na.rm = T),
     sum_score_educ = 
-          sum(education_score, na.rm = T),
+      sum(education_score, na.rm = T),
     nb_adult_no_educ = 
-          sum(ifelse(education_score <= 3, worker,0)),
+      sum(ifelse(education_score <= 3, worker,0)),
     nb_adult_mid_educ = 
-          sum(ifelse(education_score >= 4 & education_score <= 7, worker, 0)),
+      sum(ifelse(education_score >= 4 & education_score <= 7, worker, 0)),
     nb_adult_higher_educ =  
-          sum(ifelse(education_score >= 8, worker,0)),
+      sum(ifelse(education_score >= 8, worker,0)),
     official_union = 
-          max(ifelse(estadocivil3 == 1 | estadocivil4 == 1, 1, 0)),
+      max(ifelse(estadocivil3 == 1 | estadocivil4 == 1, 1, 0)),
     age_IQR = 
-          IQR(age)
-          )
+      IQR(age)
+  )
 
 ################### MERGE TEMP & GROUP ####################
 
@@ -294,25 +354,25 @@ data_temp = data_temp %>%
 
 ################## FINALIZE DATA_TEMP ####################
 
- data_temp = data_temp %>%
-   mutate(avg_score_educ = 
-                sum_score_educ / hhsize,
-          pct_adult_no_educ = 
-                nb_adult_no_educ / hhsize,
-          pct_adult_mid_educ = 
-                nb_adult_mid_educ / hhsize,
-          pct_adult_higher_educ = 
-                nb_adult_higher_educ / hhsize,
-          pct_late = 
-                ifelse(is.na(sum_rez_esc/nb_student),0,sum_rez_esc/nb_student),
-          lugar_region = 
-                ifelse(lugar1 == 1, "Central", 
-                                ifelse(lugar2 == 1, "Chorotega",
-                                ifelse(lugar3 == 1, "Pacifico Central",
+data_temp = data_temp %>%
+  mutate(avg_score_educ = 
+           sum_score_educ / hhsize,
+         pct_adult_no_educ = 
+           nb_adult_no_educ / hhsize,
+         pct_adult_mid_educ = 
+           nb_adult_mid_educ / hhsize,
+         pct_adult_higher_educ = 
+           nb_adult_higher_educ / hhsize,
+         pct_late = 
+           ifelse(is.na(sum_rez_esc/nb_student),0,sum_rez_esc/nb_student),
+         lugar_region = 
+           ifelse(lugar1 == 1, "Central", 
+                  ifelse(lugar2 == 1, "Chorotega",
+                         ifelse(lugar3 == 1, "Pacifico Central",
                                 ifelse(lugar4 == 1, "Brunca",
-                                ifelse(lugar5 == 1, "Huerta Atlantica","Huerta Norte"))))),
-          Target = 
-                factor(Target))
+                                       ifelse(lugar5 == 1, "Huerta Atlantica","Huerta Norte"))))),
+         Target = 
+           factor(Target))
 
 ##################### DATA FT ################
 ## FINAL FEATURES ON HOUSEHOLDS ##
@@ -322,3 +382,6 @@ data_ft = data_temp %>%
   select(c(1,96,143,4:7, 12, 15, 23,101,104,114,124,134:141,
            147:155,160:173,176,178:186)) 
 names(data_ft)
+
+## if you want to remove some columns from data_ft : 
+data_ft = subset(data_ft, select = -c(SQBescolari, SQBage, SQBovercrowding))

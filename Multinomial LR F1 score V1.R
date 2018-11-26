34b07f33b8664adf7576ec079a001a5d70da0c4d
lmonti91 <44442540+lmonti91@users.noreply.github.com>
2018-11-26 22:52:23 +0100

    Update Multinomial LR F1 score V1.R

--- Multinomial LR F1 score V1.R
+++ Multinomial LR F1 score V1.R
@@ -2,24 +2,46 @@
 #MULTINOMIAL LOGISTIC REGRESSION#
 #################################
 
+library(e1071)
+library(nnet)
+
+train_indices = sample(nrow(data_ft), nrow(data_ft)*0.80)
+data_train = data_ft[train_indices,]
+data_test = data_ft[-train_indices,]
+
 
 # Fit the model
-model <- nnet::multinom(Target ~., data = data_ft)
+model <- nnet::multinom(Target ~
+                          +data_train$overcrowding+
+                          data_train$edjefi+
+                          data_train$phone_per_capita+
+                          data_train$meaneduc+
+                          data_train$age_IQR, data = data_train)
 summary(model)
 
 # Make predictions
-predicted.classes <- model %>% predict(data_ft_test)
+
+predicted.classes <- model %>% predict(data_test)
 head(predicted.classes)
 
 # Model accuracy
-mean(predicted.classes == data_ft_test$Target)
+mean(predicted.classes == data_test$Target)
 
-## accuracy of 68% to describe different categories
+## F1 score
+cm = as.matrix(table(data_test$Target, predicted.classes)) #confusion matrix
+n = sum(cm) # number of instances
+nc = nrow(cm) # number of classes
+diag = diag(cm) # number of correctly classified instances per class 
+rowsums = apply(cm, 1, sum) # number of instances per class
+colsums = apply(cm, 2, sum) # number of predictions per class
+p = rowsums / n # distribution of instances over the actual classes
+q = colsums / n # distribution of instances over the predicted classes
 
-## Calcul du F1 Score
+precision = diag / colsums 
+recall = diag / rowsums 
+f1 = ifelse(is.na(2 * precision * recall / (precision + recall)),0,2 * precision * recall / (precision + recall))
+macroF1_val = sum(f1)/4
+macroF1_val
 
-library(MLmetrics)
-logreg <- glm(formula = Target ~ .,family = binoial(link = "logit"), data = data_ft)
-pred <- ifelse(logreg$fitted.values < 0.5, 0, 1)
 
-F1_Score(y_pred = pred, y_true = as.numeric(as.character(data_ft$Target), positive = "0")
+## données pas très linéaires, ce modèle n'est donc pas adapté
